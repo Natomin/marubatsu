f8d38540801643e1ddaaf146ef8cbdf584834cb9
藤村奈都美 <fujimuranatsumi@fujimurakyouotoko-no-MacBook-Air-2.local>
2016-08-30 22:43:54 +0900

    不要なstaticの削除、クラスの機能修正

--- src/gui/MarubatsuFrame.java
+++ src/gui/MarubatsuFrame.java
@@ -13,12 +13,14 @@ import marubatsu.Marubatsu;
 public class MarubatsuFrame extends JFrame {
 	public static final int BUTTON_SIZE = 120;
 	public static final int BOARD_SIZE = 3;
-	public static JButton[][] buttons = new JButton[BOARD_SIZE][BOARD_SIZE];
-	private static JLabel label = new JLabel();
-	public static int[][] buttonfrag = new int[BOARD_SIZE][BOARD_SIZE];
+	public  JButton[][] buttons = new JButton[BOARD_SIZE][BOARD_SIZE];
+	private  JLabel label = new JLabel();
+	public  int[][] buttonfrag = new int[BOARD_SIZE][BOARD_SIZE];
+	private Marubatsu marubatsu;
 
 	// コンストラクタ
 	public MarubatsuFrame() {
+		marubatsu = new Marubatsu();
 		// frameの設定
 		this.setBounds(50, 50, 375, 450);
 		this.setLayout(null);
@@ -40,24 +42,43 @@ public class MarubatsuFrame extends JFrame {
 				buttons[i][j].addActionListener(new ActionListener() {
 					@Override
 					public void actionPerformed(ActionEvent e) {
-						Marubatsu.player(x, y);// Event発生時の処理
+						marubatsu.player(x, y);// Event発生時の処理
+						update();//Marubatsuで管理している盤状態を反映
 					}
 				});
 				this.add(buttons[i][j]);
 			}
 		}
+		update();
+		playerlabel();
 	}
 
 	// プレイヤーをラベルに表示
-	public static void playerlabel() {
+	public void playerlabel() {
 		String text = null;
-		if (Marubatsu.playerfrag == Marubatsu.MARU) {
+		if (marubatsu.getPlayer() == Marubatsu.MARU) {
 			text = "次は○の番です。";
-		} else if (Marubatsu.playerfrag == Marubatsu.BATSU) {
+		} else if (marubatsu.getPlayer() == Marubatsu.BATSU) {
 			text = "次は×の番です。";
 		}
 		label.setText(text);
 		label.setFont(new Font("MS ゴシック", Font.BOLD,20));
 	}
-
+	
+	private void update(){
+		for (int i = 0; i < BOARD_SIZE; i++) {
+			for (int j = 0; j < BOARD_SIZE; j++){
+				int stat = marubatsu.getBoardStat(i, j);
+				if (stat == Marubatsu.MARU) {
+					buttons[i][j].setText("○");
+					buttons[i][j].setFont(new Font("MS ゴシック", Font.BOLD, 50));
+				} else if (stat == Marubatsu.BATSU) {
+					buttons[i][j].setText("×");
+					buttons[i][j].setFont(new Font("MS ゴシック", Font.BOLD, 70));
+				}else if(stat == Marubatsu.NONE){
+					buttons[i][j].setText(" ");
+				}
+			}
+		}
+	}
 }
--- src/marubatsu/Marubatsu.java
+++ src/marubatsu/Marubatsu.java
@@ -7,25 +7,36 @@ public class Marubatsu {
 	public static final int NONE = 0;
 	public static final int MARU = 1;
 	public static final int BATSU = 2;
-	public static int playerfrag = MARU;
-
-	public static void player(int x,  int y) {
-		if (MarubatsuFrame.buttonfrag[x][y] == NONE) {//盤上になにもないから置いてOK
-			if (playerfrag == MARU) {//○の番
-				MarubatsuFrame.buttons[x][y].setText("○");
-				MarubatsuFrame.buttons[x][y].setFont(new Font("MS ゴシック", Font.BOLD,50));
-				MarubatsuFrame.buttonfrag[x][y] = MARU;
-				playerfrag = BATSU;//次は×の番
-				MarubatsuFrame.playerlabel();
-			} else if (playerfrag == BATSU) {//×の番
-				MarubatsuFrame.buttons[x][y].setText("×");
-				MarubatsuFrame.buttons[x][y].setFont(new Font("MS ゴシック", Font.BOLD,70));
-				MarubatsuFrame.buttonfrag[x][y] = BATSU;
-				playerfrag = MARU;//次は○の番
-				MarubatsuFrame.playerlabel();
+	public int playerfrag;
+	
+	//ボード上の状態を記録しておく配列
+	private int[][] board = new int[MarubatsuFrame.BOARD_SIZE][MarubatsuFrame.BOARD_SIZE];
+	public Marubatsu(){//コンストラクタ
+		playerfrag = MARU;
+		for(int i=0; i<MarubatsuFrame.BOARD_SIZE; i++){
+			for(int j=0; j<MarubatsuFrame.BOARD_SIZE; j++){
+				board[i][j] = NONE;
 			}
 		}
 	}
+	
+	public int getPlayer(){
+		return playerfrag;
+	}
+
+	public void player(int x,  int y) {
+		if (board[x][y] == NONE){//盤上になにもないから置いてOK
+			if (playerfrag == MARU) {//○の番
+				board[x][y] = MARU;
+				playerfrag = BATSU;//次は×の番
+			}else if(playerfrag == BATSU){
+				board[x][y] = BATSU;
+				playerfrag = MARU;//次は○の番
+			}
+		}
+	}
+	
+	public int getBoardStat(int x, int y){
+		return board[x][y];
+	}
 }
-
-
